55a0623e5fce99e6533026d205fe86fdc00f354b
abdo <abdstr.homsi@gmail.com>
2026-08-27 16:12:14 +0200

    feat: add Event Grid blob trigger scaffold

--- function_app.py
+++ function_app.py
@@ -1,6 +1,21 @@
-import azure.functions as func
-import datetime
-import json
 import logging
 
+import azure.functions as func
+
+
 app = func.FunctionApp()
+
+
+@app.function_name(name="process_blob_upload")
+@app.blob_trigger(
+    arg_name="input_blob",
+    path="incoming/{name}",
+    connection="CloudOpsStorage",
+    source=func.BlobSource.EVENT_GRID,
+)
+def process_blob_upload(input_blob: func.InputStream) -> None:
+    logging.info(
+        "CloudOps received blob: name=%s size_bytes=%s",
+        input_blob.name,
+        input_blob.length,
+    )
